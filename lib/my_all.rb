b1ee99219a06f602ee07fe02232af037c3886a90
Monica Marziano <mmarziano1@gmail.com>
2018-04-03 15:10:41 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?  
+    i = 0 
+      while i < collection.length
+        yield(collection[i])
+      i += 1 
+    end 
+  end
 end
+
+
+
