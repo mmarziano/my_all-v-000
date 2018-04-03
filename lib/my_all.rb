58eabe54b52cfdf558531fb7d1c2c7cec724d9ac
Monica Marziano <mmarziano1@gmail.com>
2018-04-03 15:12:44 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,11 +9,11 @@ def my_all?(collection)
       i += 1 
     end 
   end
-end
 
-if block_return_values.include?(false)
-  false
-else
-  true 
-end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end
+end
